3735480b520d5ccd9d2b352b5a172cf57fda2684
0629469 <0629469@gmail.com>
2026-08-02 11:20:12 +0900

    Add hashing module

---
+++ src/hashing.py
@@ -0,0 +1,9 @@
+import hashlib
+
+
+def calculate_hash(path, chunk_size=1024 * 1024):
+    h = hashlib.sha256()
+    with open(path, "rb") as f:
+        while chunk := f.read(chunk_size):
+            h.update(chunk)
+    return h.hexdigest()
